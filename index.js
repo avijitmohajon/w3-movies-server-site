16ec133d8a9dc5648408c4c66d3fb3d431a454d9
avijit mohajon <avijitmohajon72@gmail.com>
2025-02-17 13:13:40 +0600

    last

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
+require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
-require("dotenv").config();
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
@@ -8,8 +8,10 @@ const port = process.env.PORT || 5000;
 // middleWare
 app.use(cors());
 app.use(express.json());
+app.get(['/*.ico', '/*.png'], (req, res) => res.status(204).end());
 
-const uri = `mongodb+srv://${process.env.USER_db}:${process.env.PASS_db}@cluster0.wye4e.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+const uri = `mongodb+srv://${process.env.USER_DB}:${process.env.PASS_DB}@cluster0.wye4e.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -23,27 +25,27 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
 
     const moviesCollection = client.db("moviesDatabase").collection("movie");
 
-    app.get("/updatemovies", async (req, res) => {
+    app.get("/movie", async (req, res) => {
       const result = await moviesCollection.find().toArray();
       res.send(result);
     });
 
     // for update information
 
-    app.get("/updatemovies/:id", async (req, res) => {
+    app.get("/movie/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
       const result = await moviesCollection.findOne(query);
       res.send(result);
     });
 
-    app.post("/updatemovies", async (req, res) => {
+    app.post("/movie", async (req, res) => {
       const newMovie = req.body;
       // console.log(newMovie);
       const result = await moviesCollection.insertOne(newMovie);
@@ -51,7 +53,7 @@ async function run() {
     });
 
     // update data
-    app.put("/updatemovies/:id", async (req, res) => {
+    app.put("/movie/:id", async (req, res) => {
       const id = req.params.id;
       const filter = { _id: new ObjectId(id) };
       const options = { upsert: true };
@@ -72,7 +74,7 @@ async function run() {
       res.send(result);
     });
 
-    app.delete("/updatemovies/:id", async (req, res) => {
+    app.delete("/movie/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
       const result = await moviesCollection.deleteOne(query);
